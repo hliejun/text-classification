b17bde8788d3e1e17a8d9f7bea4e37f34e5b2752
Huang Lie Jun <liejun.hlj@gmail.com>
2017-11-11 23:46:55 +0800

    Initialised project

---
+++ tc-test.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class tc_test {
+    public static void main(String[] args) {
+        // Hello world ...
+    }
+}
---
+++ tc-train.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class tc_train {
+    public static void main(String[] args) {
+        // Hello world ...
+    }
+}
