ebf4b63a81ce93784b7c9719d0034f4b95e377f9
Huang Lie Jun <liejun.hlj@gmail.com>
2017-11-12 15:19:43 +0800

    Added todos

--- tc-test.java
+++ tc-test.java
@@ -1,7 +1,25 @@
 import java.util.*;
 
 public class tc_test {
+    // Takes in stopword-list, model, test-list, test-class-list
     public static void main(String[] args) {
-        // Hello world ...
+        // PRE-PROCESS
+            // 1. Remove the following:
+                // a) frequently occurring stop words
+                // b) words occurring less than k times in the training texts
+                // c) numbers
+                // d) punctuation symbols
+            // 2. Stem words using Porter-Stemmer
+
+        // FEATURE SELECTION
+            // 3. Calculate chi-square values for each stemmed word in text
+            // 4. Calculate normalised frequencies of selected features for text vector
+
+        // CLASSIFY
+            // 5. Dot product with each perceptron classifier weight vector
+            // 6. Label text with the class whose weight vector gives the greatest dot product value
+
+        // OUTPUT RESULTS
+            // 7. Output labelled text to results file
     }
 }
--- tc-train.java
+++ tc-train.java
@@ -1,7 +1,32 @@
 import java.util.*;
 
 public class tc_train {
+    // Takes in stopword-list, train-class-list, model
     public static void main(String[] args) {
-        // Hello world ...
+        // PRE-PROCESS
+            // 1. Remove the following:
+                // a) frequently occurring stop words
+                // b) words occurring less than k times in the training texts
+                // c) numbers
+                // d) punctuation symbols
+            // 2. Stem words using Porter-Stemmer
+
+        // FEATURE SELECTION
+            // 3. Calculate chi-square values for each stemmed word in text
+            // 4. Calculate normalised frequencies of selected features for text vector
+
+        // LEARN PERCEPTION CLASSIFIERS (ASSUMING SEPARABILITY)
+            // 5. Separate training data by classes
+            // 6. For each class, learn the perceptron classifier based on text vectors
+                // a) set initial weight (incl. x0, w0)
+                // b) iterate through N samples (do we do passes too?)
+                    // i) classify sample with every weight vector (dot product)
+                    // ii) after classification, run training rule to update weight vectors
+                // c) obtain converged weight vector for each class
+
+        // OUTPUT PERCEPTRON CLASSIFIERS
+            // 7. Output labelled perceptron classifiers to model file
+
+
     }
 }
